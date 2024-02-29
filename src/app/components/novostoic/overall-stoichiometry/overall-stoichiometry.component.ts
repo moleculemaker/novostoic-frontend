a75ec80814264581ede8f7002892ea9962a8f9b7
ckouder <bingjiguo@icloud.com>
2024-02-29 14:02:14 -0600

    add overall stoichiometry page

---
+++ src/app/components/novostoic/overall-stoichiometry/overall-stoichiometry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+@Component({
+  selector: "app-overall-stoichiometry",
+  templateUrl: "./overall-stoichiometry.component.html",
+  styleUrls: ["./overall-stoichiometry.component.scss"],
+  host: {
+    class: "grow",
+  },
+})
+export class OverallStoichiometryComponent {}
