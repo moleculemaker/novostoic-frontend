c15de35963b6cc6af3704d9beffbf6ddea7be27d
ckouder <bingjiguo@icloud.com>
2024-03-06 10:10:17 -0600

    chore: add overall stoichiometry

--- src/app/components/novostoic/overall-stoichiometry-result/overall-stoichiometry-result.component.ts
+++ src/app/components/novostoic/overall-stoichiometry-result/overall-stoichiometry-result.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-overall-stoichiometry-result',
-  templateUrl: './overall-stoichiometry-result.component.html',
-  styleUrls: ['./overall-stoichiometry-result.component.scss']
+  selector: "app-overall-stoichiometry-result",
+  templateUrl: "./overall-stoichiometry-result.component.html",
+  styleUrls: ["./overall-stoichiometry-result.component.scss"],
+  host: {
+    class: "grow",
+  },
 })
-export class OverallStoichiometryResultComponent {
-
-}
+export class OverallStoichiometryResultComponent {}
