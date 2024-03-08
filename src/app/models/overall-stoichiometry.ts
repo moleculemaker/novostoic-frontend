1aa9cd6704baac47d3e6c0d28cd92bcc37aa36f2
ckouder <bingjiguo@icloud.com>
2024-03-08 13:26:20 -0600

    chore: update data structure

--- src/app/models/overall-stoichiometry.ts
+++ src/app/models/overall-stoichiometry.ts
@@ -1,4 +1,5 @@
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { JobCreate } from "../api/mmli-backend/v1";
 
 export class OverallStoichiometryRequest {
   form = new FormGroup({
@@ -7,36 +8,48 @@ export class OverallStoichiometryRequest {
     agreeToSubscription: new FormControl(false),
     subscriberEmail: new FormControl("", [Validators.email]),
   });
+
+  toJobCreate(): JobCreate {
+    return {
+      job_info: JSON.stringify({
+        primaryPrecursor: this.form.controls["primaryPrecursor"].value,
+        targetMolecule: this.form.controls["targetMolecule"].value,
+      }),
+      email: this.form.controls["subscriberEmail"].value || "",
+      job_id: undefined,
+      run_id: 0,
+    };
+  }
+}
+
+interface NovostoicMolecule {
+  commonNames: string[]; // one molecule might have multiple common names
+  smiles: string;
+  keggId?: string;
+  structure?: string; // svg or data url to display molecule structure
 }
 
 export class OverallStoichiometryResponse {
-  status: "LOADING" | "FINISHED" | "CANCELLED";
-  jobId: string;
-  submissionTime: Date;
-  percentFinished: number;
-  estimatedFinish: Date;
-  primaryPrecursor: {
-    smiles: string;
-    commonName: string;
-    keggId: string;
-  };
-  targetMolecule: {
-    smiles: string;
-    commonName: string;
-    keggId: string;
-  };
-  result: {
-    page: number;
-    totalPages: number;
-    numResultPerPage: number;
-    totalResults: number;
-    result: {
-      stoichiometry: {
-        reactants: { name: string; amount: string }[];
-        yield: { name: string; amount: string }[];
-      };
-      yield: number;
-      deltaG: number;
-    }[];
-  };
+  /** The following are included in jobs service
+   * status: "LOADING" | "FINISHED" | "CANCELLED";
+   * jobId: string;
+   * submissionTime: Date;
+
+   * Not existing yet
+   * percentFinished: number;
+   * estimatedFinish: Date;
+   */
+
+  primaryPrecursor: NovostoicMolecule;
+  targetMolecule: NovostoicMolecule;
+
+  // return all results at once, having pagination will increase burden
+  results: Array<{
+    stoichiometry: {
+      reactants: Array<{ molecule: NovostoicMolecule; amount: number }>;
+      products: Array<{ molecule: NovostoicMolecule; amount: number }>;
+    };
+    yield: number;
+    deltaG: number;
+  }>;
 }
