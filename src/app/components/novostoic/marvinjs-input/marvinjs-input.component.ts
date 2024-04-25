59f1f13f217b75c786714fc6b0d7a75a6c69563e
ckouder <bingjiguo@icloud.com>
2024-04-25 15:58:30 -0500

    use debounce on validation

--- src/app/components/novostoic/marvinjs-input/marvinjs-input.component.ts
+++ src/app/components/novostoic/marvinjs-input/marvinjs-input.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output, forwardRef } from "@angular/core";
 import { AbstractControl, AsyncValidator, ControlValueAccessor, NG_ASYNC_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from "@angular/forms";
-import { BehaviorSubject, Observable, debounce, debounceTime, map } from "rxjs";
+import { BehaviorSubject, Observable, debounce, debounceTime, filter, map, switchMap, take } from "rxjs";
 import { NovostoicService } from "~/app/services/novostoic.service";
 
 @Component({
@@ -33,6 +33,18 @@ export class MarvinjsInputComponent implements ControlValueAccessor, AsyncValida
     this._value = value;
     this.onChange(value);
     this.onTouched();
+  }
+
+  _smiles = "";
+  get smiles() {
+    return this._smiles;
+  }
+
+  set smiles(value: string) {
+    this._smiles = value;
+    if (this.value !== value) {
+      this.value = value;
+    }
   }
 
   showDialog$ = new BehaviorSubject(false);
@@ -68,17 +80,26 @@ export class MarvinjsInputComponent implements ControlValueAccessor, AsyncValida
   onValidatorChange = () => {};
 
   validate(control: AbstractControl<any, any>): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-      return this.novostoicService.getChemicalAutoComplete(control.value)
-        .pipe(
-            map((chemicals) => chemicals.some((chemical) => 
-                chemical.name === control.value
-                || chemical.inchi === control.value
-                || chemical.inchi_key === control.value
-                || chemical.smiles === control.value
-                || chemical.metanetx_id === control.value
-                || chemical.kegg_id === control.value
-            ) ? null : {chemicalNotSupported: true}),
-        );
+      return control.valueChanges.pipe(
+        debounceTime(500),
+        switchMap((v) => this.novostoicService.getChemicalAutoComplete(v)),
+        map((chemicals) => {
+          const chemical = chemicals.find((chemical) => 
+            chemical.name === control.value
+            || chemical.inchi === control.value
+            || chemical.inchi_key === control.value
+            || chemical.smiles === control.value
+            || chemical.metanetx_id === control.value
+            || chemical.kegg_id === control.value
+          );
+          if (chemical) {
+            this.smiles = chemical.smiles;
+            return null;
+          }
+          return { chemicalNotSupported: true };
+        }),
+        take(1),
+      );
   }
 
   registerOnValidatorChange?(fn: () => void): void {
