f071023cde81d8951f1cfa32c0885d619acc95aa
ckouder <bingjiguo@icloud.com>
2024-06-10 14:49:11 -0700

    fix enzrank input

--- src/app/models/enz-rank.ts
+++ src/app/models/enz-rank.ts
@@ -1,5 +1,6 @@
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { NovostoicMolecule } from "./overall-stoichiometry";
+import { ChemicalAutoCompleteResponse } from "../api/mmli-backend/v1";
 
 export class EnzymeSelectionRequest {
   form = new FormGroup({
@@ -9,9 +10,19 @@ export class EnzymeSelectionRequest {
     subscriberEmail: new FormControl("", [Validators.email]),
   });
 
+  primaryPrecursor: ChemicalAutoCompleteResponse | null;
+
   static example() {
     const request = new EnzymeSelectionRequest();
     request.form.controls["primaryPrecursor"].setValue("O[C@@H](CC(O)=O)C(O)=O");
+    request.primaryPrecursor = {
+      "name": "(S)-malate",
+      "smiles": "O=C(O)C[C@H](O)C(=O)O",
+      "inchi": "InChI=1S/C4H6O5/c5-2(4(8)9)1-3(6)7/h2,5H,1H2,(H,6,7)(H,8,9)/p-2/t2-/m0/s1",
+      "inchi_key": "InChIKey=BJEPYKJPYRNKOW-REOHCLBHSA-L",
+      "metanetx_id": "MNXM1107192",
+      "kegg_id": "C00149",
+    }
     request.form.controls["enzymeSequence"].setValue(
       "A0A4P8WFA8:MTKRVLVTGGAGFLGSHLCERLLSEGHEVICLDNFGSGRRKNIKEFEDHPSFKVNDRDVRISESLPSVDRIYHLASRASPADFTQFPVNIALANTQGTRRLLDQARACDARMVFASTSEVYGDPKVHPQPETYTGNVNIRGARGCYDESKRFGETLTVAYQRKYDVDARTVRIFNTYGPRMRPDDGRVVPTFVTQALRGDDLTIYGDGEQTRSFCYVDDLIEGLISLMRVDNPEHNVYNIGKENERTIKELAYEVLGLTDTESDIVYEPLPEDDPGQRRPDITRAKTELDWEPKISLREGLEDTITYFDN",
     );
@@ -20,11 +31,20 @@ export class EnzymeSelectionRequest {
 
   clearAll() {
     Object.values(this.form.controls).forEach((control) => control.reset());
+    this.primaryPrecursor = null;
+  }
+
+  setPrimaryPrecursor(molecule: ChemicalAutoCompleteResponse) {
+    this.primaryPrecursor = molecule;
   }
 
   toRequestBody() {
+    if (!this.primaryPrecursor) {
+      throw new Error("Primary precursor not set");
+    }
     const jobInfo = {
-      primary_precursor: this.form.controls["primaryPrecursor"].value || '',
+      primary_precursor: 'N00001',
+      add_info: `N00001:${this.primaryPrecursor.smiles}`,
       enzyme_sequences: [this.form.controls["enzymeSequence"].value?.replaceAll('\\n', '') || ''],
     };
     return {
