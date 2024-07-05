20e56c0149f853fa64efed0645339a7fb42c07e2
ckouder <bingjiguo@icloud.com>
2024-07-05 14:01:24 -0500

    show error when job failed

--- src/app/components/novostoic/loading/loading.component.ts
+++ src/app/components/novostoic/loading/loading.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
-import { combineLatest, filter, interval, map, of } from "rxjs";
+import { BehaviorSubject, combineLatest, filter, interval, map, of } from "rxjs";
 import { JobStatus, JobType } from "~/app/api/mmli-backend/v1";
 import { JobResult } from "~/app/models/job-result";
 import { NovostoicService } from "~/app/services/novostoic.service";
@@ -27,6 +28,12 @@ export class LoadingComponent extends JobResult implements OnInit {
 
   estimatedTime: number;
   estimatedTimeString: string;
+  showError$ = new BehaviorSubject(false);
+
+  form = new FormGroup({
+    agreeToSubscription: new FormControl(false),
+    subscriberEmail: new FormControl("", [Validators.required, Validators.email]),
+  });
 
   value$ = combineLatest([
     this.statusResponse$,
@@ -34,30 +41,33 @@ export class LoadingComponent extends JobResult implements OnInit {
   ]).pipe(
     map(([status, _]) => {
       //TODO: queued job should return 0, wait for backend to fix the status error
-      if (status.phase === JobStatus.Processing
-        || status.phase === JobStatus.Queued
-      ) {
-        const jobStartedMs = (status.time_created || 0) * 1000; // use created time for estimation for now
-        const jobElapsedMs = Date.now() - jobStartedMs;
-        const coe = 2 / 5;
-        const estimatedMs = this.estimatedTime * 1000;
-        const decimalTime = (jobElapsedMs % estimatedMs) / estimatedMs;
-        const wholeTime = Math.floor(jobElapsedMs / estimatedMs);
+      switch (status.phase) {
 
-        let sum = 0;
-        let i = 1;
-        while (i < wholeTime && wholeTime >= 1) {
-          sum += Math.pow(coe, i);
-          i++;
-        }
-        sum += Math.pow(coe, i) * decimalTime;
-        return sum * 100;
-      } 
+        case JobStatus.Queued:
+        case JobStatus.Processing:
+          const jobStartedMs = (status.time_created || 0) * 1000; // use created time for estimation for now
+          const jobElapsedMs = Date.now() - jobStartedMs;
+          const coe = 2 / 5;
+          const estimatedMs = this.estimatedTime * 1000;
+          const decimalTime = (jobElapsedMs % estimatedMs) / estimatedMs;
+          const wholeTime = Math.floor(jobElapsedMs / estimatedMs);
 
-      else if (status.phase === JobStatus.Completed) {
-        return 100;
+          let sum = 0;
+          let i = 1;
+          while (i < wholeTime && wholeTime >= 1) {
+            sum += Math.pow(coe, i);
+            i++;
+          }
+          sum += Math.pow(coe, i) * decimalTime;
+          return sum * 100;
+
+        case JobStatus.Completed:
+          return 100;
+
+        case JobStatus.Error:
+          this.showError$.next(true);
+          return 0;
       }
-
       return 0;
     }
   ))
@@ -83,5 +93,17 @@ export class LoadingComponent extends JobResult implements OnInit {
     }
     
     this.estimatedTimeString = resultString;
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+    
+    this.novostoicService.updateSubscriberEmail(
+      this.jobType,
+      this.jobId, 
+      this.form.controls["subscriberEmail"].value || ''
+    );
   }
 }
