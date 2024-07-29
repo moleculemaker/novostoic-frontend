58bc62c03a2d944189a53b4b081fda1e241dfde0
ckouder <bingjiguo@icloud.com>
2024-07-29 12:21:30 -0500

    allow reuse existing novel molecule

--- src/app/models/dg-predictor.ts
+++ src/app/models/dg-predictor.ts
@@ -1,10 +1,11 @@
 import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import { debounceTime } from "rxjs";
 
 interface ReactionFormControl {
   type: FormControl<string | null>;
   containNovelMolecule?: FormControl<boolean | null>;
   reactionSmiles?: FormControl<string | null>;
-  novelMolecules?: FormArray<FormControl<string | null>>;
+  novelMolecules?: FormArray<FormGroup>;
   reactionKeggId?: FormControl<string | null>;
 }
 
@@ -20,6 +21,7 @@ export class ThermodynamicalFeasibilityRequest {
     reactionInputType: new FormControl("keggId", [Validators.required]),
   });
 
+  private uniqueNovelMolecule = new Set();
   private createReactionSmilesFormControl() {
     return new FormGroup<ReactionFormControl>({
       type: new FormControl("smiles"),
@@ -31,20 +33,16 @@ export class ThermodynamicalFeasibilityRequest {
     const formGroup = new FormGroup<ReactionFormControl>({
       type: new FormControl("keggId"),
       containNovelMolecule: new FormControl(false),
-      novelMolecules: new FormArray([
-        new FormControl(""),
-      ]),
+      novelMolecules: new FormArray([] as any[]),
       reactionKeggId: new FormControl("", [Validators.required]),
     });
 
     formGroup.controls["containNovelMolecule"]?.valueChanges.subscribe((value) => {
       if (value) {
-        formGroup.controls["novelMolecules"]?.controls.forEach((control) => {
-          control.setValidators([Validators.required]);
-        });
+        this.addNovelMolecule(formGroup);
       } else {
-        formGroup.controls["novelMolecules"]?.controls.forEach((control) => {
-          control.clearValidators();
+        formGroup.controls["novelMolecules"]?.controls.forEach((_, i) => {
+          this.removeNovelMolecule(formGroup, i);
         });
       }
       formGroup.controls["novelMolecules"]?.controls.forEach((control) => {
@@ -82,7 +80,10 @@ export class ThermodynamicalFeasibilityRequest {
           type: "keggId",
           containNovelMolecule: true,
           novelMolecules: [
-            "InChI=1S/C14H12O/c15-14-8-4-7-13(11-14)10-9-12-5-2-1-3-6-12/h1-11,15H/b10-9+"
+            {
+              alias: "N00001",
+              value: "InChI=1S/C14H12O/c15-14-8-4-7-13(11-14)10-9-12-5-2-1-3-6-12/h1-11,15H/b10-9+"
+            }
           ],
           reactionKeggId: "C01745 + C00004 <=> N00001 + C00003 + C00001",
         }
@@ -111,12 +112,55 @@ export class ThermodynamicalFeasibilityRequest {
   }
 
   addNovelMolecule(subform: FormGroup<ReactionFormControl>) {
-    subform.controls['novelMolecules']!.push(
-      new FormControl("", [Validators.required])
-    );
+    const group = new FormGroup({
+      iid: new FormControl(`iid-${Math.random()}`),
+      alias: new FormControl("", [Validators.required]),
+      value: new FormControl("", [Validators.required]),
+      moleculeExisted: new FormControl(false),
+    })
+
+    // listen to value changes and update the molecule index
+    group.controls['value'].valueChanges
+      .subscribe((value) => {
+        let aliasSet = false;
+        
+        this.uniqueNovelMolecule.clear();
+        group.controls['moleculeExisted'].setValue(false);
+
+        this.form.controls["reactions"].controls.forEach((reaction) => {
+          reaction.controls['novelMolecules']?.controls.forEach((g) => {
+            if (g.controls['iid'] === group.controls['iid']) {
+              return;
+            }
+
+            const moleculeValue = g.controls['value'].value;
+            this.uniqueNovelMolecule.add(moleculeValue);
+            if (moleculeValue === value) {
+              group.controls['alias'].setValue(g.controls['alias'].value);
+              group.controls['moleculeExisted'].setValue(true);
+              aliasSet = true;
+            }
+          });
+        });
+
+        if (!aliasSet) {
+          const moleculeIndex = `${this.uniqueNovelMolecule.size + 1}`.padStart(5, "0");
+          group.controls['alias'].setValue(`N${moleculeIndex}`);
+        }
+      });
+
+      const moleculeIndex = `${this.uniqueNovelMolecule.size + 1}`.padStart(5, "0");
+      group.controls['alias'].setValue(`N${moleculeIndex}`);
+
+    subform.controls['novelMolecules']!.push(group);
   }
 
   removeNovelMolecule(subform: FormGroup<ReactionFormControl>, idx: number) {
+    subform.controls["novelMolecules"]?.controls.forEach((group) => {
+      group.controls['value'].clearValidators();
+      group.controls['alias'].clearValidators();
+    });
+
     subform.controls['novelMolecules']!.removeAt(idx);
     if (!subform.controls['novelMolecules']!.length) {
       this.addNovelMolecule(subform);
@@ -141,9 +185,8 @@ export class ThermodynamicalFeasibilityRequest {
 
         if (reaction.containNovelMolecule) {
           payload['add_info'] = {};
-          reaction.novelMolecules?.forEach((molecule, i) => {
-            const key = `${i + 1}`.padStart(5, "0");
-            payload["add_info"][`N${key}`] = molecule;
+          reaction.novelMolecules?.forEach((group, i) => {
+            payload["add_info"][group.alias] = group.value;
           });
         }
 
