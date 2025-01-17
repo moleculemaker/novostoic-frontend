c1c8631cb80b2b17128261c4c650e8239f4d0ad2
ckouder <bingjiguo@icloud.com>
2025-01-17 15:15:40 -0600

    overall-stoichiometry precompute example

--- src/app/components/novostoic/overall-stoichiometry/overall-stoichiometry.component.ts
+++ src/app/components/novostoic/overall-stoichiometry/overall-stoichiometry.component.ts
@@ -21,15 +21,16 @@ export class OverallStoichiometryComponent {
   validatedPrimaryPrecursor$ = new BehaviorSubject<ChemicalAutoCompleteResponse | null>(null);
   validatedTargetMolecule$ = new BehaviorSubject<ChemicalAutoCompleteResponse | null>(null);
 
+  exampleJobId = "5a3cbcdca72d4fdaab33603725bb9ef8";
+  examplePrimaryPrecursor = "C00022";
+  exampleTargetMolecule = "C01013";
+  examplePrimaryPrecursorUsed = false;
+  exampleTargetMoleculeUsed = false;
+
   constructor(
     private router: Router,
-    private sanitizer: DomSanitizer,
     private novostoicService: NovostoicService,
   ) {}
-
-  useExample() {
-    this.request = OverallStoichiometryRequest.useExample(this.novostoicService);
-  }
 
   useExampleValue(control: string, value: string) {
     this.request.form.get(control)!.setValue(value);
@@ -39,8 +40,23 @@ export class OverallStoichiometryComponent {
     this.showDialog$.next(false);
   }
 
+  onPrimaryPrecursorValidated(response: ChemicalAutoCompleteResponse | null) {
+    this.validatedPrimaryPrecursor$.next(response);
+    this.examplePrimaryPrecursorUsed = response?.kegg_id === this.examplePrimaryPrecursor;
+  }
+
+  onTargetMoleculeValidated(response: ChemicalAutoCompleteResponse | null) {
+    this.validatedTargetMolecule$.next(response);
+    this.exampleTargetMoleculeUsed = response?.kegg_id === this.exampleTargetMolecule;
+  }
+
   onSubmit() {
     if (!this.request.form.valid) {
+      return;
+    }
+
+    if (this.examplePrimaryPrecursorUsed && this.exampleTargetMoleculeUsed) {
+      this.router.navigate([NovostoicTools.OVERALL_STOICHIOMETRY, "result", this.exampleJobId]);
       return;
     }
 
