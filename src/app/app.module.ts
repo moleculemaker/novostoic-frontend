68c609c4f534c86f356d3308f22f1422ba748215
matthewberry <github@mjberry.com>
2026-02-12 20:26:36 -0600

    update Matomo URL from mmli1 to mmli2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -154,7 +154,7 @@ const initAppFn = (envService: EnvironmentService) => {
     HttpClientModule,
     NgxMatomoTrackerModule.forRoot({
       siteId: 7,
-      trackerUrl: "https://matomo.mmli1.ncsa.illinois.edu/",
+      trackerUrl: "https://matomo.mmli2.ncsa.illinois.edu/",
     }),
     NgxMatomoRouterModule,
     MenuModule,
